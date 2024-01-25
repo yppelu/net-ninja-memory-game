d221bbed6288924f0483bc019dd315e160f4d71e
yppelu <yppelu@gmail.com>
2024-01-25 13:45:16 +0700

    Fix bug when few pictures are used

--- helpers/getCards.js
+++ helpers/getCards.js
@@ -4,7 +4,7 @@ export default function getCards() {
   const cards = [];
   const usedIndexes = [];
   while (cards.length < 12) {
-    const index = Math.floor(Math.random() * cards.length);
+    const index = Math.floor(Math.random() * images.length);
     if (!usedIndexes.includes(index)) {
       cards.push({ ...images[index], id: Math.random(), matched: false });
       cards.push({ ...images[index], id: Math.random(), matched: false });
